e7cb55bb1c952128b9783a3a16d768bc283017c8
Kendall Easterly <qfletcher89@gmail.com>
2024-02-01 11:05:37 -0600

    deprecated disc, removing it by intercepting requests using data from localstorage

--- app/Models/TrackModel.ts
+++ app/Models/TrackModel.ts
@@ -17,6 +17,7 @@ export function useTrackModel() {
 
 		const track = JSON.parse(JSON.stringify(rawTrack))
 		delete track.dateAdded
+		delete track.disc //we deprecated disc
 
 		return new Promise((resolve, reject) => {
 			// const serverURL = "http://localhost:4000"
@@ -25,8 +26,13 @@ export function useTrackModel() {
 
 			function fetchNewDownloadURL(): Promise<PlaybackObject> {
 
+				console.log("fetching download url", track.id)
+
 				return new Promise((resolve, reject) => {
 					const payload = JSON.stringify(track);
+
+					console.log({track})
+
 					axios
 						.post(
 							serverURL + "/metadata-link" + `?sender=${account.uid}`,
